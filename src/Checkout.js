8ec202565f9bfaf427ed6a987bc0471ad4e5eb06
Christopher <kriss17388+dev@gmail.com>
2023-02-07 15:37:19 +0000

    formatting

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,22 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import {
   PaymentRequestButtonElement,
   useStripe,
-} from '@stripe/react-stripe-js';
-import axios from 'axios';
+} from "@stripe/react-stripe-js";
+import axios from "axios";
 
-const Checkout = props => {
+const Checkout = (props) => {
   const stripe = useStripe();
   const [paymentRequest, setPaymentRequest] = useState(null);
-  const [paymentSuccess, setPaymentSuccess] = useState(false);
+  // const [paymentSuccess, setPaymentSuccess] = useState(false);
 
   useEffect(() => {
     if (stripe) {
       const pr = stripe.paymentRequest({
-        country: 'US',
-        currency: 'usd',
+        country: "US",
+        currency: "usd",
         total: {
-          label: 'Purchase',
+          label: "Purchase",
           amount: 100,
         },
         requestPayerName: true,
@@ -25,7 +25,7 @@ const Checkout = props => {
         googlePay: true,
       });
 
-      pr.canMakePayment().then(result => {
+      pr.canMakePayment().then((result) => {
         if (result) {
           setPaymentRequest(pr);
         }
@@ -34,18 +34,18 @@ const Checkout = props => {
   }, [stripe]);
 
   if (paymentRequest) {
-    paymentRequest.on('paymentmethod', async ev => {
+    paymentRequest.on("paymentmethod", async (ev) => {
       const response = await axios.post(
-        'https://secret-harbor-59973.herokuapp.com/create-payment-intent/',
+        "https://secret-harbor-59973.herokuapp.com/create-payment-intent/",
         {
           amount: 100,
           automatic_payment_methods: {
             enabled: true,
           },
-          currency: 'usd',
+          currency: "usd",
         }
       );
-      console.log('response data: ', response.data);
+      console.log("response data: ", response.data);
       const clientSecret = response.data.client_secret;
 
       // Confirm the PaymentIntent without handling potential next actions (yet).
@@ -60,19 +60,18 @@ const Checkout = props => {
         // Report to the browser that the payment failed, prompting it to
         // re-show the payment interface, or show an error message and close
         // the payment interface.
-        ev.complete('fail');
+        ev.complete("fail");
       } else {
         // Report to the browser that the confirmation was successful, prompting
         // it to close the browser payment method collection interface.
-        ev.complete('success');
-        console.log('success');
-        // <Text>SUCCESS</Text>;
-        setPaymentSuccess(true);
-        console.log("payment success: ", paymentSuccess)
+        ev.complete("success");
+        console.log("success");
+        // setPaymentSuccess(true);
+        // console.log("payment success: ", paymentSuccess)
         // Check if the PaymentIntent requires any actions and if so let Stripe.js
         // handle the flow. If using an API version older than "2019-02-11"
         // instead check for: `paymentIntent.status === "requires_source_action"`.
-        if (paymentIntent.status === 'requires_action') {
+        if (paymentIntent.status === "requires_action") {
           // Let Stripe.js handle the rest of the payment flow.
           const { error } = await stripe.confirmCardPayment(clientSecret);
           if (error) {
